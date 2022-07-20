b8e700ff746c242c7ff1afbab0f81846f9865f1d
Karun K Joy <80326355+KarunKJ@users.noreply.github.com>
2022-07-20 20:47:18 +0400

    Create single_route.js

---
+++ single_route.js
@@ -0,0 +1,11 @@
+//making a simple route for connection in the backend using express
+var express = require('express');
+var app = express();
+ 
+app.get("/", function(req, res){
+ res.send("HELLO FROM OUR WEB APP!");
+});
+ 
+app.listen(8080, function () {
+ console.log('App listening on port 8080!');
+});
