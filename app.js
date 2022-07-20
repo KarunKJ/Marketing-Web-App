d4a8ad3f129346ec6c9916ecd2fe59d53d9d645b
Karun K Joy <80326355+KarunKJ@users.noreply.github.com>
2022-07-20 20:45:55 +0400

    Update app.js

--- app.js
+++ app.js
@@ -26,3 +26,15 @@ connection.query(q, [data], function(err, result) {
 });
  
 connection.end();
+
+//making a simple route for connection in the backend using express
+var express = require('express');
+var app = express();
+ 
+app.get("/", function(req, res){
+ res.send("HELLO FROM OUR WEB APP!");
+});
+ 
+app.listen(8080, function () {
+ console.log('App listening on port 8080!');
+});
