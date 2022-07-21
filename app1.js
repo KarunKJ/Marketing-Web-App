ce100b6f3c33ed38600a76bf182ca3d9a5cd2296
Karun K Joy <80326355+KarunKJ@users.noreply.github.com>
2022-07-21 10:12:53 +0400

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 var mysql = require('mysql');
 
 app.set("view engine" , "ejs");
-
+app.use(bodyParser.urlencoded({extended : true}));
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -21,6 +22,16 @@ app.get("/" , function(req , res){
 		res.render("home", {data : count} );
 	});
 });
+
+
+app.post('/register', function(req,res){
+ var person = {email: req.body.email};
+ connection.query('INSERT INTO users SET ?', person, function(err, result) {
+ console.log(err);
+ console.log(result);
+ res.redirect("/");
+ });
+})
 
 
 app.listen(3000 , function(){
